b85a9ad8baabbf57c8612b06a94ff6f77b35c4ca
Sonya Yuyukina <yuyukinasofia@gmail.com>
2026-04-16 19:59:16 +0700

    move token to env file

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
+
 BASE_URL = "https://cloud-api.yandex.net/v1/disk"
-OAUTH_TOKEN = "y0__xCgooPmCBi-rUAg2cHnixcwgcvoiwjn5IzrlwGaopbLS-KIFGXocte6bw"
 TEST_FILES_DIR = "tests/test_files/"
 
+OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
+
+if not OAUTH_TOKEN:
+    raise ValueError("Токен не найден")
